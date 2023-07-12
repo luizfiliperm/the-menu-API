14681375f67f3318770bf55d685912a77c2892d7
luizfiliperm <luizfilipemr17@gmail.com>
2023-07-12 16:40:29 -0300

    Mapping Items and Categories

--- src/main/java/com/jf/menu/entities/Category.java
+++ src/main/java/com/jf/menu/entities/Category.java
@@ -1,12 +1,18 @@
 package com.jf.menu.entities;
 
+import java.util.List;
+
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
 
 @Entity
+@Table(name = "category")
 public class Category {
     
     @Id
@@ -19,4 +25,51 @@ public class Category {
 
     @Column(name = "description")
     private String description;
+
+    @OneToMany(mappedBy = "category",cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<Item> items;
+
+	public Category() {
+	}
+
+	public Category(String name, String description) {
+		this.name = name;
+		this.description = description;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public List<Item> getItems() {
+		return items;
+	}
+
+	public void setItems(List<Item> items) {
+		this.items = items;
+	}
+
+    public void addItem(Item item){
+        this.items.add(item);
+    }
 }
--- src/main/java/com/jf/menu/entities/Item.java
+++ src/main/java/com/jf/menu/entities/Item.java
@@ -5,8 +5,12 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
 
 @Entity
+@Table(name = "item")
 public class Item {
     
     @Id
@@ -25,6 +29,10 @@ public class Item {
 
     @Column(name = "img_url")
     private String imgUrl;
+
+    @ManyToOne
+    @JoinColumn(name = "category_id", nullable = false)
+    private Category category;
 
     public Item() {
     }
