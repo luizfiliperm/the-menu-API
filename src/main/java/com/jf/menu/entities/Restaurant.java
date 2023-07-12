16887cf4cc38a7de97eb04cc14641a3222ab6fdc
luizfiliperm <luizfilipemr17@gmail.com>
2023-07-12 13:34:26 -0300

    Restaurant Entity

---
+++ src/main/java/com/jf/menu/entities/Restaurant.java
@@ -0,0 +1,80 @@
+package com.jf.menu.entities;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Restaurant {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "adress")
+    private String adress;
+
+    @Column(name = "phone_number")
+    private String phoneNumber;
+
+    @Column(name = "logo_url")
+    private String logoUrl;
+
+    public Restaurant() {
+    }
+
+    public Restaurant(String name, String adress, String phoneNumber, String logoUrl) {
+        this.name = name;
+        this.adress = adress;
+        this.phoneNumber = phoneNumber;
+        this.logoUrl = logoUrl;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAdress() {
+        return adress;
+    }
+
+    public void setAdress(String adress) {
+        this.adress = adress;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getLogoUrl() {
+        return logoUrl;
+    }
+
+    public void setLogoUrl(String logoUrl) {
+        this.logoUrl = logoUrl;
+    }
+
+    
+}
