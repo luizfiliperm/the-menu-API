fc0e702a99b0c59b826c4c82ffa249c72f5e9388
luizfiliperm <luizfilipemr17@gmail.com>
2023-07-27 15:51:53 -0300

    SecurityConfiguration

--- src/main/java/com/jf/menu/infra/security/SecurityFilter.java
+++ src/main/java/com/jf/menu/infra/security/SecurityFilter.java
@@ -26,8 +26,8 @@ public class SecurityFilter extends OncePerRequestFilter {
     AuthorizationService authorizationService;
 
     @Override
-    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
-            throws ServletException, IOException {
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) 
+        throws ServletException, IOException {
         var token = this.recoverToken(request);
         if(token != null){
             var subject = this.tokenService.validateToken(token);
